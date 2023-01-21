63eabfd33279e3d92769c1f4ac747c946077891b
Alexandr Dylevich <dylevich220885@gmail.com>
2023-01-21 22:55:07 +0300

    modified App.js and App.css less 11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+        <div className="app-wrapper">
+            <header className='header'>
+                <img src={logo}></img>
             </header>
+            <nav className='nav'>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Message
+                </div>
+                <div>
+                    News
+                </div>
+                <div>
+                    Music
+                </div>
+                <div>
+                    Settings
+                </div>
+            </nav>
+            <div className='content'>
+                Main content
+                <div>
+                    <img src="https://i1.mybook.io/p/x480/bookset/0e/4e/0e4e921f-8dd7-4ba2-834d-7b1161992f83.png"></img>
+                </div>
+                <div>
+                    avatar
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+
+            </div>
+
         </div>
     );
 }
